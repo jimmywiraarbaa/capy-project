8c8173f5da269154b3405ae4cdbfa65b00820c04
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-19 18:41:31 +0700

    not clear hero description

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function tampilkanSelengkapnya() {
+    var detailTextShort = document.getElementById("detail-text-short");
+    var detailTextFull = document.getElementById("detail-text-full");
+    var readMoreBtn = document.getElementById("read-more-btn");
+    detailTextFull.style.display = "block";
+}
