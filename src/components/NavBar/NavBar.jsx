f22bd635d0dd7d9c73c4d9e67e55cc6e75135879
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-22 11:21:54 +0700

    Navbar Done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,89 +1,43 @@
-// import React from 'react'
-// import "./NavBar.css";
-import React from "react";
+import { useState } from 'react';
+import Logo from '../../../assets/Logo.png';
+import './NavBar.css';
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-function Navbar() {
   return (
-    <nav
-      className="bg-white shadow-lg"
-      style={{ position: "fixed", width: "100%" }}
-    >
-      <div className="container mx-auto">
-        <div className="flex justify-between items-center px-4 py-3">
-          <a
-            className="text-xl font-bold text-gray-800"
-            href="#"
-            data-aos="fade-right"
-          >
-            Nexus
+    <nav className="flex items-center justify-between flex-wrap">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <img src={Logo} alt="Logo" className="h-8 w-auto" />
+      </div>
+      <div className="block lg:hidden">
+        <button onClick={() => setIsOpen(!isOpen)} className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+          <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+        </button>
+      </div>
+      <div className={`w-full block ${isOpen ? 'block' : 'hidden'} lg:flex lg:items-center lg:w-auto lg:justify-center`}>
+        <div className="list-nav text-sm lg:flex-grow">
+          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0  hover:text-white ">
+            Beranda
           </a>
-          <button className="block lg:hidden focus:outline-none" type="button">
-            <svg
-              className="h-6 w-6 text-gray-600"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16m-7 6h7"
-              />
-            </svg>
-          </button>
-          <div className="hidden lg:flex lg:items-center lg:w-auto">
-            <div className="text-sm">
-              <a
-                href="#"
-                className="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-gray-800 mr-10"
-                data-aos="zoom-in"
-              >
-                Beranda
-              </a>
-              <a
-                href="#"
-                className="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-gray-800 mr-10"
-                data-aos="zoom-in"
-              >
-                Bengkel
-              </a>
-              <a
-                href="#"
-                className="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-gray-800 mr-10"
-                data-aos="zoom-in"
-              >
-                Suku Cadang
-              </a>
-              <a
-                href="#"
-                className="block mt-4 lg:inline-block lg:mt-0 text-gray-600 hover:text-gray-800"
-                data-aos="zoom-in"
-              >
-                FaQ
-              </a>
-            </div>
-            <div className="text-sm lg:flex-grow">
-              <a
-                href="#"
-                className="block lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600 mr-4"
-                data-aos="fade-left"
-              >
-                Daftar
-              </a>
-              <a
-                href="#"
-                className="block lg:inline-block lg:mt-0 text-gray-800 hover:text-gray-600"
-                data-aos="fade-left"
-              >
-                Masuk
-              </a>
-            </div>
-          </div>
+          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0  hover:text-white ">
+            Bengkel
+          </a>
+          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0  hover:text-white ">
+            Suku Cadang
+          </a>
+          <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0  hover:text-white ">
+            FaQ
+          </a>
         </div>
+      </div>
+      <div className={`w-full block ${isOpen ? 'block' : 'hidden'} lg:flex lg:items-center lg:w-auto lg:justify-center`}>
+      <div className="buttons flex items-center">
+        <a href="#" className="inline-block text-sm px-4 py-2 leading-none border    hover:border-transparent hover:text-teal-500  mt-4 lg:mt-0">Daftar</a>
+        <a href="#" className="login inline-block text-sm px-4 py-2 leading-none  hover:bg-blue-600 mt-4 lg:mt-0">Masuk</a>
+      </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
