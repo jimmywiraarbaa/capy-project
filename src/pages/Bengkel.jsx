bfdc2ae21ba09b1f10a88272e62f7bc482c39b72
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-28 16:33:44 +0700

    bengkel responsive

--- src/pages/Bengkel.jsx
+++ src/pages/Bengkel.jsx
@@ -2,73 +2,55 @@
 import NavBar from "../components/NavBar/NavBar";
 import Bar from "../components/bar/bar";
 import Footer from "../components/Footer/Footer";
+import Gambar1 from '../../assets/bengkel8.jpg'
 
 function Bengkel() {
   return (
     <div>
       <NavBar />
-      <br />
-      <Bar />
-      <br />
-      <div className="bar flex  border-b-2 border-black mx-8 mb-8 gap-6">
-        <div className=" bar p-2  hover:border-black hover:border-b-8">
-          <a className="" href="#">
-            Galeri
-          </a>
-        </div>
-        <div className="p-2 hover:border-black hover:border-b-8">
-          <a className="" href="#">
-            Suku Cadang
-          </a>
-        </div>
-        <div className="p-2 hover:border-black hover:border-b-8">
-          <a className="" href="#">
-            Ulasan
-          </a>
-        </div>
-      </div>
-      {/* ======================= Gambar ========================================================== */}
-      <div className="container mx-auto text-center">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {/* First image */}
-          <div className="col-span-1">
-            <img
-              src="/assets/aneka motor 6.jpg"
-              alt="."
-              className="w-full rounded-lg hover:opacity-75"
-            />
-          </div>
 
-          {/* Second image */}
-          <div className="col-span-1">
-            <img
-              src="./assets/aneka motor 3.jpg"
-              alt=""
-              className="w-full rounded-lg hover:opacity-75"
-            />
+      <div className="px-10">
+        <Bar />
+        <div className="bar flex  border-b-2 border-info-color mb-8 gap-8 max-sm:mb-5 max-sm:gap-5">
+          <div className=" bar p-2  border-primary-color border-b-8">
+            <a className="font-bold" href="#">
+              Galeri
+            </a>
           </div>
-
-          {/* Third image */}
-          <div className="col-span-1">
-            <img
-              src="./assets/aneka motor 4.png"
-              alt=""
-              className="w-full rounded-lg hover:opacity-75"
-            />
+          <div className="p-2">
+            <a className="" href="#">
+              Suku Cadang
+            </a>
           </div>
-
-          {/* Fourth image */}
-          <div className="col-span-1">
-            <img
-              src="./assets/aneka motor 5.jpg"
-              alt=""
-              className="w-full rounded-lg hover:opacity-75"
-            />
+          <div className="p-2">
+            <a className="" href="#">
+              Ulasan
+            </a>
+          </div>
+        </div>
+        {/* ======================= Gambar ========================================================== */}
+        <div className="galery">
+          <div className="w-full">
+            <img className="" src={Gambar1} alt="" />
+          </div>
+          <div className="flex">
+            <div>
+              <img src={Gambar1} alt="" />
+            </div>
+            <div className="flex">
+              <div className="w-full">
+                <img className="w-1/2" src={Gambar1} alt="" />
+                <img className="w-1/2" src={Gambar1} alt="" />
+              </div>
+              <div className="w-full">
+                <img className="w-1/2" src={Gambar1} alt="" />
+                <img className="w-1/2" src={Gambar1} alt="" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <br />
-      <br />
+
       <Footer />
     </div>
   );
