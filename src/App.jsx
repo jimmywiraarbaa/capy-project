ce60f870796f1dbbdd64911bb04c95ed3f8f4fe8
Rerey Maulana <144551736+Rerey155-del@users.noreply.github.com>
2024-03-03 00:40:33 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,9 @@ import MitraTinjau from "./pages/mitra_tinjau";
 import BengkelMitra from "./pages/Bengkel_mitra";
 import SukuCadang from "./pages/sukucadang_mitra";
 import UlasanBengkel from "./pages/BengkelUlasan";
-
+import SukuCadangUser from "./pages/sukucadang_user";
+import BengkelList from "./pages/bengkel_list";
+import SukuCadangList from "./pages/sukucadang_list";
 
 function App() {
   return (
@@ -39,14 +41,17 @@ function App() {
           <Route path="/" element={<Home />} />
 
           <Route path="/bengkel" element={<Bengkel />} />
+          <Route path="/sukucadanguser" element={<SukuCadangUser />} />
 
           <Route path="/bengkelmitra" element={<BengkelMitra />} />
           <Route path="/sukucadangmitra" element={<SukuCadang />} />
           <Route path="/ulasanmitra" element={<UlasanBengkel />} />
 
-
           <Route path="/profile" element={<Profile />} />
           <Route path="/riwayat" element={<Riwayat />} />
+
+          <Route path="/bengkellist" element={<BengkelList />} />
+          <Route path="/sukucadanglist" element={<SukuCadangList />} />
         </Routes>
       </Router>
     </>
